1db1aaaddca1cf433eaf29becb4d735aab340177
DayNoone <dihelgoy@gmail.com>
2014-10-17 13:10:22 +0200

    Correct file

--- oving_4/simulatedannealing.py
+++ oving_4/simulatedannealing.py
@@ -1,137 +1,184 @@
 import numpy as np
-import copy
 import math
 import random
-
+import copy
+ 
 # Simulated Annealing
-def objective_function(list):
-	penalty
-
-	#Calculating row penalty
-	for row in xrange(len(list[0])):
-		rowValue = 0
-		for column in xrange(len(list)):
-			if list[row][column] == 1:
-				rowValue += 1
-		if rowValue > K:
-			penalty += 1
-
-	#Calculating column penalty
-	for column in xrange(len(list)):
-		columnValue = 0
-		for row in xrange(len(list[0])):
-			if list[row][column] == 1:
-				columnValue += 1
-		if columnValue > K:
-			penalty += 1
-
-	#Calculating diagonal value
-	matrix = np.array(list)
-	diags = [matrix[::-1,:].diagonal(i) for i in range(-matrix.shape[0]+1,matrix.shape[1])]
-	diags.extend(matrix.diagonal(i) for i in range(matrix.shape[1]-1,-matrix.shape[0],-1))
-	for n in diags:
-		if sum(n) > K:
-			penalty += 1
-
-	rating =  1.0/(1.0+penalty)
-	return rating
-
-def generateNeighbors(p, currentNodeX, currentNodeY):
-	neighbors = []
-	path = []
-
-	for i in range(4):
-		child = copy.deepcopy(p)
-		ydy = currentNodeY + dy[i]
-		xdx = currentNodeX + dx[i]
-		
-		if ydy >= len(p) or xdx >= len(p[0]) or ydy < 0 or xdx < 0:
-			continue
-		
-		if child[ydy][xdx] == 0:
-			child[ydy][xdx] = 1
-		elif child[ydy][xdx] == 1:
-			child[ydy][xdx] = 0
-		if objective_function(child) != -1:
-			neighbors.append(child)
-		path.append(i)
-	print len(neighbors)
-	return neighbors, path
-
-def findBestNeighbor(neighbors, path):
-	bestValue = -100
-	pMax = None 
-	newdx, newdy = 0, 0
-	for i in range(len(neighbors)):
-		tempValue = objective_function(neighbors[i])
-		if tempValue > bestValue:
-			bestNeighbor = neighbors[i]
-			bestValue = tempValue
-			newdx = dx[path[i]]
-			newdy = dy[path[i]]
-	return bestNeighbor, newdx, newdy
-
+def objective_function(p):
+    penalty = 0
+ 
+    # Calculating row penalty
+    for row in xrange(len(p[0])):
+        rowValue = 0
+        for column in xrange(len(p)):
+            if p[row][column] == 1:
+                rowValue += 1
+        if rowValue > K:
+            penalty += 1
+ 
+    #Calculating column penalty
+    for column in xrange(len(p)):
+        columnValue = 0
+        for row in xrange(len(p[0])):
+            if p[row][column] == 1:
+                columnValue += 1
+        if columnValue > K:
+            penalty += 1
+ 
+    #Calculating diagonal value
+    matrix = np.array(p)
+    diags = [matrix[::-1, :].diagonal(i) for i in range(-matrix.shape[0] + 1, matrix.shape[1])]
+    diags.extend(matrix.diagonal(i) for i in range(matrix.shape[1] - 1, -matrix.shape[0], -1))
+    for n in diags:
+        if sum(n) > K:
+            penalty += 1
+ 
+    rating = 1.0 / (1.0 + penalty)
+    return rating
+ 
+ 
+def generateNeighbors(p):
+    neighbors = []
+    conflictEggs = []
+ 
+    #Generating neighbors for problem-rows
+    for row in range(len(p)):
+        #rowValue is set to amount of eggs in row
+        rowValue = 0
+        for column in xrange(len(p[0])):
+            if p[row][column] == 1:
+                rowValue += 1
+                conflictEggs.append([row, column])
+        #Breaking loop when first conflict-row is found
+        if rowValue > K:
+            break
+        else:
+        #Clear conflict-eggs when the row is not a conflict-row
+            conflictEggs = []
+ 
+    for i in conflictEggs:
+        for row in range(len(p[0])):
+            child = copy.deepcopy(p)
+ 
+            #Remove conflict node
+            child[i[0]][i[1]] = 0
+            if child[row][i[1]] == 0 and row != i[0]:
+                child[row][i[1]] = 1
+                neighbors.append(child)
+ 
+    #Clearing conflict-eggs
+    conflictEggs = []
+ 
+    #Generating neighbors for problem-columns
+    for column in range(len(p[0])):
+        #columnValue is set to amount of eggs in column
+        columnValue = 0
+        for row in range(len(p)):
+            if p[row][column] == 1:
+                columnValue += 1
+                conflictEggs.append([row, column])
+        #Breaking loop when first conflict-column is found
+        if columnValue > K:
+            break
+        #Clear conflict-eggs when the column is not a conflict-column
+        else:
+            conflictEggs = []
+ 
+    for i in conflictEggs:
+        for column in range(len(p[0])):
+            child = copy.deepcopy(p)
+ 
+            #Remove conflict node
+            child[i[0]][i[1]] = 0
+            if child[i[0]][column] == 0 and column != i[1]:
+                child[i[0]][column] = 1
+                neighbors.append(child)
+ 
+    if len(neighbors) == 0:
+        neighbors.append(generateRandomBoard(N, K*N))
+    return neighbors
+ 
+ 
+def findBestNeighbor(neighbors):
+    bestValue = 0
+    pMax = None
+    for i in range(len(neighbors)):
+        tempValue = objective_function(neighbors[i])
+        if tempValue > bestValue:
+            bestValue = tempValue
+            pMax = neighbors[i]
+    return pMax
+ 
+ 
 def getQ(state, bestNeighbor):
-	valueState = objective_function(state)
-	if valueState == 0:
-		return 0
-	return (objective_function(bestNeighbor) - valueState)/valueState
-
+    valueState = objective_function(state)
+    if valueState == 0:
+        return 0
+    return (objective_function(bestNeighbor) - valueState) / valueState
+ 
+ 
 def getNewP(q, T):
-	p = math.exp(-q/T)
-	if p < 1:
-		return p
-	else:
-		return 1
-
+    return min(1, math.exp(-q / T))
+ 
+ 
+def generateRandomBoard(sizeOfBoard, noOfEggs):
+    eggsPlaced = 0
+    board = [[0 for i in range(sizeOfBoard)] for j in range(sizeOfBoard)]
+    while eggsPlaced < noOfEggs:
+        x, y = random.randint(0, sizeOfBoard-1), random.randint(0, sizeOfBoard-1)
+        if board[y][x] == 0:
+            board[y][x] = 1
+            eggsPlaced += 1
+    return board
+ 
+ 
 def SA():
-	state = [[0 for x in xrange(N)] for y in xrange(N)]
-
-	temp = tempMax
-
-	currentNodeX = 0
-	currentNodeY = 0
-
-	while temp > 0:
-		if state != None:
-			for a in state:
-				print a                
-		print objective_function(state)
-		print currentNodeX, currentNodeY
-
-		print
-		if objective_function(state) >= fTarget:
-			print 'Optimal solution'
-			return state
-		else:
-			neighbors, path = generateNeighbors(state, currentNodeX, currentNodeY)
-			pMax, currentdx, currentdy = findBestNeighbor(neighbors, path)
-			currentNodeX += currentdx
-			currentNodeY += currentdy
-
-
-			q = getQ(state, pMax)
-			
-			p = getNewP(q, temp)
-
-			x = random.randint(0, fTarget)
-			
-			if x > p:
-				state = pMax
-			else:
-				neighbors.remove(pMax)
-				p = neighbors[random.randint(0, len(neighbors)-1)]
-
-			temp -= dT
-
+    state = generateRandomBoard(N, K*N)
+    temp = tempMax
+ 
+    while temp > 0.02:
+        if objective_function(state) == 1:
+            for a in state:
+                print a
+            print objective_function(state)
+            print
+            return 1
+ 
+        else:
+            neighbors = generateNeighbors(state)
+            pMax = findBestNeighbor(neighbors)
+ 
+            q = getQ(state, pMax)
+ 
+            p = getNewP(q, temp)
+ 
+            x = random.randint(0, fTarget)
+ 
+            if x > p:
+                state = pMax
+            else:
+                if len(neighbors) == 1:
+                    p = neighbors[0]
+                else:
+                    neighbors.remove(pMax)
+                    p = neighbors[random.randint(0, len(neighbors) - 1)]
+ 
+            temp -= dT
+    for a in state:
+        print a
+    print objective_function(state)
+    print
+ 
+ 
 # MAIN
-dx = [1, 1, 0, -1, -1, -1, 0, 1]
-dy = [0, 1, 1, 1, 0, -1, -1, -1]
-N = 5
-M = 5
-K = 2
-tempMax = 1.0
-fTarget = 10
+N = 10
+M = N
+K = 3
+tempMax = 10.0
+fTarget = 10.0
 dT = 0.02
-
-SA()
+counter = 0
+for i in range(50):
+    if SA() == 1:
+        counter += 1
+print counter
